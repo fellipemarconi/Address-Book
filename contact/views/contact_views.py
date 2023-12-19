fdd87b8d625378557ce5280207f4f9f64623308b
Fellipe <anksrr@proton.me>
2023-12-18 22:45:48 -0300

    I created a variable in index view that there're a list of contacts also i created a context with this variable and i made the table in index.html after this i extended this index to principal index in the app contact

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from contact.models import Contact
 
 def index(request):
-    return render(request, 'contact/index.html')
+    contacts = Contact.objects.filter(show=True).order_by('-id')
+    
+    context = {
+        'contacts': contacts,
+    }
+    
+    return render(request, 'contact/index.html', context=context)
