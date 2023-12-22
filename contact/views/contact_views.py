92752b4a7c2cbf77830143ceb59291fe92207968
Fellipe <anksrr@proton.me>
2023-12-22 20:39:57 -0300

    I created partials _header.html and _head.html also I included this in base.html

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -16,8 +16,11 @@ def contact(request, contact_id):
         Contact.objects, pk=contact_id, show=True
         )
     
+    contact_name = f'{single_contact.first_name} {single_contact.last_name} - '
+    
     context = {
         'contact': single_contact,
+        'site_title': contact_name,
     }
     
     return render(request, 'contact/contact.html', context)
