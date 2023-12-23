352780514d192d8925ed3178d8038fda50b8f4fe
Fellipe <anksrr@proton.me>
2023-12-22 22:14:24 -0300

    I'll use a pattern CRUD in views to contact

--- contact/urls.py
+++ contact/urls.py
@@ -6,7 +6,9 @@ from contact import views
 app_name = 'contact'
 
 urlpatterns = [
-    path('<int:contact_id>/', views.contact, name='contact'),
     path('search/', views.search, name='search'),
     path('', views.index, name='index'),
+    
+    # contact (CRUD)
+    path('contact/<int:contact_id>/', views.contact, name='contact'),
 ]
