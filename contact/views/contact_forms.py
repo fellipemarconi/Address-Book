fcb63664d8edc0e7928a263d0db4c3c2487229dd
Fellipe <anksrr@proton.me>
2023-12-25 23:41:39 -0300

    I created a for in create.html to create a dinamic form also I created a class ContactForm to create a Django form and I added it to view create

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from contact.models import Contact
+from django import forms
 from django.http import Http404
 from django.db.models import Q
 from django.core.paginator import Paginator
 
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = ('first_name', 'last_name', 'phone', 'email')
+
 def create(request):
-    context = {}
+    if request.method == 'POST':
+        
+        context = {
+            'form': ContactForm(request.POST),
+        }
+    
+        return render(request, 'contact/create.html', context=context)
+    
+    context = {
+            'form': ContactForm(request.POST),
+        }
     
     return render(request, 'contact/create.html', context=context)
