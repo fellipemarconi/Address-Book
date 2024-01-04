d52b6fdd1e0e9f7634cb073535d3951a930f7340
Fellipe <anksrr@proton.me>
2024-01-03 22:29:24 -0300

    Button create, url update, view update I created a button in _header that go to contact/create, I created a url update with contact_id and I created a logic in view update to edited tha same contact_id that it was create

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,23 +1,55 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from contact.forms import ContactForm
+from django.urls import reverse
+from contact.models import Contact
 
 def create(request):
+    form_action = reverse('contact:create')
+    
     if request.method == 'POST':
         
         form = ContactForm(request.POST)
         
         context = {
             'form': form,
+            'form_action': form_action
         }
 
         if form.is_valid():
-            form.save()
-            return redirect('contact:create')
+            contact = form.save()
+            return redirect('contact:update', contact_id=contact.pk)
                 
         return render(request, 'contact/create.html', context=context)
     
     context = {
             'form': ContactForm(),
+            'form_action': form_action
+        }
+    
+    return render(request, 'contact/create.html', context=context)
+
+def update(request, contact_id):
+    contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    form_action = reverse('contact:update', args=(contact_id,))
+    
+    if request.method == 'POST':
+        
+        form = ContactForm(request.POST, instance=contact)
+        
+        context = {
+            'form': form,
+            'form_action': form_action
+        }
+
+        if form.is_valid():
+            contact = form.save()
+            return redirect('contact:update', contact_id=contact.pk)
+                
+        return render(request, 'contact/create.html', context=context)
+    
+    context = {
+            'form': ContactForm(instance=contact),
+            'form_action': form_action
         }
     
     return render(request, 'contact/create.html', context=context)
