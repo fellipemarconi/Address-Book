49e44e18a7fa4102b5cfe5384872238b6d492b90
Fellipe <anksrr@proton.me>
2024-01-07 17:51:05 -0300

    I added the decorator @login_required to views: user_update, logout_view, contact (create, update, delete)

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, redirect, get_object_or_404
 from contact.forms import ContactForm
 from django.urls import reverse
 from contact.models import Contact
+from django.contrib.auth.decorators import login_required
 
+@login_required(login_url='contact:login')
 def create(request):
     form_action = reverse('contact:create')
     
@@ -28,6 +30,7 @@ def create(request):
     
     return render(request, 'contact/create.html', context=context)
 
+@login_required(login_url='contact:login')
 def update(request, contact_id):
     contact = get_object_or_404(Contact, pk=contact_id, show=True)
     form_action = reverse('contact:update', args=(contact_id,))
@@ -54,6 +57,7 @@ def update(request, contact_id):
     
     return render(request, 'contact/create.html', context=context)
 
+@login_required(login_url='contact:login')
 def delete(request, contact_id):
     contact = get_object_or_404(Contact, pk=contact_id, show=True)
     
--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
 from contact.forms import RegisterForm, RegisterUpdateForm
 from django.contrib import messages, auth
+from django.contrib.auth.decorators import login_required
 
 def register(request):
     form = RegisterForm()
@@ -38,10 +39,12 @@ def login_view(request):
     
     return render(request,'contact/login.html',context)
 
+@login_required(login_url='contact:login')
 def logout_view(request):
     auth.logout(request)
     return redirect('contact:login')
 
+@login_required(login_url='contact:login')
 def user_update(request):
     form = RegisterUpdateForm(instance=request.user)
     
